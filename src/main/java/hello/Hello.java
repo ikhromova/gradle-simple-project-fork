f6c8b2becda2a42a7257eeb7c9bdb7890982a40f
Irina Khromova <irina.khromova@jetbrains.com>
2024-09-10 18:19:46 +0300

    Add more hardcoded passwords

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -4,6 +4,12 @@ import java.util.stream.Stream;
 
 
 public class Hello {
+    public String password = "t";
+    public String  name = "xoxp-12345678901-987654321012-987654321012-1234123412341234567890abcdef12345";
+    public String  password2 = "pa#sw@rd;";
+    public String  password3 = "pa#sw@rd;";
+    public String  p = "t";
+    public String  t = "[\"123:AAabcde1234567890ABCDEFghijkl-_mnopqrs\"]";
 
     public static void main(String[] args) {
         if (true) {
@@ -20,6 +26,11 @@ public class Hello {
 
         if (false) {
             System.out.println("x");
+        }
+
+
+        if (false) {
+
         }
     }
 }
