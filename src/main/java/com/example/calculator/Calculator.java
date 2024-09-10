c9c1b8f214aba1b64f1ab64999a528b6968b6ac4
Irina Khromova <irina.khromova@jetbrains.com>
2024-09-10 14:15:19 +0300

    Add hardcoded passwords

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -2,6 +2,12 @@ package com.example.calculator;
 
 
 public class Calculator {
+    public String password = "t";
+    public String  name = "xoxp-12345678901-987654321012-987654321012-1234123412341234567890abcdef12345";
+    public String  password2 = "pa#sw@rd;";
+    public String  password3 = "pa#sw@rd;";
+    public String  p = "t";
+    public String  t = "[\"123:AAabcde1234567890ABCDEFghijkl-_mnopqrs\"]";
 
     public int add(int a, int b) {
         return a + b;
@@ -23,6 +29,19 @@ public class Calculator {
     }
 
     public int devideee(String a, int b) {
+        return a.indexOf('d') + b;
+    }
+
+    public int deviasddeee(String a, int b) {
+        return a.indexOf('d') + b;
+    }
+
+    public int deviasdadsfdfdeee(String a, int b) {
+        return a.indexOf('d') + b;
+    }
+
+    public int wfsdfsdf(String a, int b) {
+
         return a.indexOf('d') + b;
     }
 
