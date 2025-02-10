30f02fe37d4d9b0dce3714983e5b5417fe898478
Irina Khromova <irina.khromova@jetbrains.com>
2025-02-10 15:51:49 +0200

    --apply-fixes and severityThreshold = any 1

--- src/main/java/holly/Holly.java
+++ src/main/java/holly/Holly.java
@@ -11,6 +11,12 @@ public class Holly {
     public static String stroong = "sdfkmsd";
 
     public static Void funSuper() {
+        if (true) {
+
+        } else if (true) {
+
+        }
+        return null;
     }
 
 
@@ -82,5 +88,8 @@ public class Holly {
 
 
     public static void main(String[] args) {
+        if (true) {
+
+        }
     }
 }
