2abe7a0791922c5a97b491795c9a31af3d48c9c4
Irina Khromova <irina.khromova@jetbrains.com>
2025-02-10 16:06:09 +0200

    Add mistakes to the files and new pr mode github wokflow

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -8,6 +8,9 @@ public class Hello {
     public static void main(String[] args) {
         if (true) {
 
+        } else if (true) {
+            int x = 4;
+            int y = x == 10 ? 4 : 4;
         }
     }
 }
--- src/main/java/holly/Holly.java
+++ src/main/java/holly/Holly.java
@@ -1,6 +1,9 @@
 package holly;
 
 import java.util.stream.Stream;
+import java.util.stream.Stream;
+import java.util.stream.Stream;
+import static java.util.stream.Stream.of;
 
 
 public class Holly {
@@ -8,6 +11,9 @@ public class Holly {
     public static void main(String[] args) {
         if (true) {
 
+        } else if (true) {
+            int x = 4;
+            int y = x == 10 ? 4 : 4;
         }
     }
 }
