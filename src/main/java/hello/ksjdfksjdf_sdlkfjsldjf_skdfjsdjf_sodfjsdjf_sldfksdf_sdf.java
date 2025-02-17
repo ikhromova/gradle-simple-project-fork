cbc63dfd7e4ba1480b0ef65984fd9c892015c2b1
Irina Khromova <irina.khromova@jetbrains.com>
2025-02-17 12:47:50 +0200

    add sanity problems

---
+++ src/main/java/hello/ksjdfksjdf_sdlkfjsldjf_skdfjsdjf_sodfjsdjf_sldfksdf_sdf.java
@@ -0,0 +1,13 @@
+package hello;
+
+
+public class ksjdfksjdf_sdlkfjsldjf_skdfjsdjf_sodfjsdjf_sldfksdf_sdf {
+
+    public static void main(String[] args) {
+    }
+    sdfsldfk
+    sdf;ksjdfksjdf_sdlkfjsldjf_skdfjsdjf_sodfjsdjf_sldfksdf_sdf
+
+
+    sfdsdf
+}
